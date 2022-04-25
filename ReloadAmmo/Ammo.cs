5548f3752c6719dff456d33b51cd03a8b77e0317
syuan7492 <kurosaki7492@gmail.com>
2022-04-26 01:15:23 +0800

    Add project files.

---
+++ ReloadAmmo/Ammo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReloadAmmo
+{
+    class Ammo
+    {
+        public const int AMMO_SIZE = 5;
+
+        private int bullets = 0;
+        private int bulletsLoaded = 0;
+        public bool IsEmpty() { return bulletsLoaded == 0; }
+        public int BulletsAmount
+        {
+            get { return bullets; }
+
+            set
+            {
+                if (value > 0)
+                    bullets = value;
+                //ReloadAmmo();
+            }
+        }
+        public int BulletsLoadedAmount
+        {
+            get { return bulletsLoaded; }
+            set { bulletsLoaded = value; }
+        }
+         
+        
+        
+        public void ReloadAmmo()
+        {
+            if (bullets >= AMMO_SIZE)
+            {
+                bulletsLoaded = AMMO_SIZE;
+                bullets -= bulletsLoaded;
+            }
+
+            else return;       
+        }
+        //當槍隻有bulletsLoaded時，回傳true並遞減bulletsLoaded欄位 
+        public bool Shoot()
+        {
+            if (bulletsLoaded == 0) return false;
+            bulletsLoaded--;
+            return true;
+        }
+    }
+}
---
+++ ReloadAmmo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReloadAmmo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ammo gunAmmo = new Ammo();
+            while (true)
+            {
+                Console.WriteLine($"\n擁有 {gunAmmo.BulletsAmount}發子彈, 彈匣內有 {gunAmmo.BulletsLoadedAmount} / 5發子彈");
+                if (gunAmmo.IsEmpty()) Console.WriteLine("警告: 彈匣已經空了!!");
+                Console.WriteLine("\n按空白鍵射擊, 按o取得5發子彈, 按p消耗5發子彈補滿彈匣, 按u離開遊戲");
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == ' ') Console.WriteLine($"\n射擊!!{gunAmmo.Shoot()}");
+                else if (key == 'o') gunAmmo.BulletsAmount += Ammo.AMMO_SIZE; 
+                else if (key == 'p') gunAmmo.ReloadAmmo();
+                else if (key == 'u') return;
+            }
+            
+        }
+    }
+}
